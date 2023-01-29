94d7974f30a32a07e4ddf9f398f24ac70dd1a000
tg-man <92434888+tg-man@users.noreply.github.com>
2023-01-29 17:49:10 +0100

    updates

--- Wrappers/Archived.m
+++ Wrappers/Archived.m
@@ -151,6 +151,226 @@ ylabel('Log of amplitude (microV)'); set(gca, 'FontSize', 14);
 xticklabels({'P8 Osc', 'P8 Non-Osc', 'P9 Osc', 'P9 Non-Osc', 'P10 Osc', 'P10 Non-Osc'}); 
 
 
+%% Ms Bio 2013
+
+
+clear
+% load experiments and generic stuff
+experiments = get_experiment_redux; %function that pulls experimental indicies from your excel file
+experiments = experiments([45]); % what experiments to keep
+repeatCalc = 1;
+save_data = 1;
+cores = 4;
+% the number of CPU cores to use to load signal. Unless you are doing
+% nothing else on your computer, do not use all of them. Otherwise you will
+% end with no processing power for anything else
+
+% loading and filtering variables
+fs = 32000; % sampling rate from data
+downsampling_factor = 160; % downsample for LFP analysis
+fs_LFP = fs / downsampling_factor;
+high_cut = fs_LFP / 2; % nyquist frequency: the number of data points per second. Max high cut should be half of sampling frequency
+ExtractMode = 1; % extract from neuralynx into matlab
+ch2load = 1:32; % select channels to load
+
+% oscillation detection variables
+freq_band = [4 20]; % frequencies on which to detect oscillations. Originally 20
+rel_thresholds = [1 2]; % relative low/peak threshold for oscillation detection (stDev)
+abs_thresholds = [50 100]; % absolute low/peak threshold for oscillation detection (microV)
+abs_thresholds_TH = [35 80]; % same as above but for TH area
+threshold_artifact = [2000 8]; 
+durations = [1000 300]; %  min inter-osc duration and min osc duration
+verbose = 0;
+
+% PSD variables
+freq2analyze = logspace(-1, log10(100), 42); % frequencies to analyze with windows of different length. Originally (-2, log10(50),20)
+slow_freqs = [0.2 5]; % slow frequencies (0.01 is the digital filter of our recordings, unless you changed it!)
+fast_freqs = [5 100]; % fast frequencies. High cut at 50 originally
+
+% folders to save various results
+folder4osc = 'Q:\Personal\Tony\MScBio_20130117\osc\'; %Fill in path where to save oscillations
+folder4PSD = 'Q:\Personal\Tony\MScBio_20130117\psd\'; %Fill in path where to save PSD
+
+
+for exp_idx = 1 : size(experiments, 2)
+    
+    % select experiment
+    experiment = experiments(exp_idx);
+    
+    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    %%%%%%%%%%%%%%%%%%%%%%%%%% LFP analysis %%%%%%%%%%%%%%%%%%%%%%%%%%
+    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    
+    % load signal and filter for LFP
+    disp(['Loading LFP for animal number ' num2str(exp_idx)])
+    parfor (channel = ch2load, cores)
+        disp(['Loading channel ' num2str(channel)]);
+        file_to_load = [experiment.path, experiment.name, '\CSC', num2str(channel), '.ncs'];
+        [~, signal, ~] = load_nlx_Modes(file_to_load, ExtractMode, []);
+        signal = ZeroPhaseFilter(signal, fs, [0.5 high_cut]); % origianlly 0.1
+        LFP(channel, :) = signal(1 : downsampling_factor : end);
+    end
+    clear signal
+    noisy_channels = experiment.NoisyCh;
+    noisy_channels = noisy_channels(ismember(noisy_channels, ch2load));
+    LFP(noisy_channels, :) = NaN; % Do the same here for noisy channels
+    
+    %%% this is here as an example of how to limit baseline analysis to
+    %%% pre opto periods, in case your file has opto stimulations. you can
+    %%% delete this part if you are only recording baseline
+    
+    % load stimulation properties
+    %%load([folder4StimProp, experiment.animal_ID, '/StimulationProperties'])
+    % extract first stimulation and convert it to samp freq used for LFP
+    %%FirstStim = round((StimulationProperties{1, 10} - fs) / downsampling_factor);
+    % only take baseline part of LFP
+    %%LFP = LFP(:, 1 : FirstStim);
+    
+    % keep only good part of the recording
+    if ~ nnz(isnan(experiment.RecKeep))
+        experiment.RecKeep = str2num(experiment.RecKeep);
+        LFP = LFP(:, experiment.RecKeep(1) * fs_LFP : ...
+            experiment.RecKeep(2) * fs_LFP);
+    end
+    
+    % take median of all channels
+    LFP4osc_PFC = nanmedian(LFP(17:32,:));
+    LFP4osc_Str = nanmedian(LFP(1:16,:));
+
+    % plot median of LFP for visual check
+    figure; 
+    subplot(2,1,1); plot(LFP4osc_PFC); title('PFC');
+    subplot(2,1,2); plot(LFP4osc_Str); title('Str');
+    sgtitle(['mouse ' experiment.animal_ID]);
+    saveas(gcf, fullfile(folder4osc, experiment.animal_ID))
+    
+    % detect oscillations
+    disp(['detecting oscillations for animal number ' num2str(exp_idx)])
+    osc_PFC = getOscillations(experiment, LFP4osc_PFC, fs_LFP, freq_band, rel_thresholds, ...
+        abs_thresholds, threshold_artifact, durations, save_data, repeatCalc, folder4osc, 1, experiment.Area1, verbose);     
+    osc_Str = getOscillations(experiment, LFP4osc_Str, fs_LFP, freq_band, rel_thresholds, ...
+        abs_thresholds, threshold_artifact, durations, save_data, repeatCalc, folder4osc, 1, experiment.Area2, verbose); 
+    clear LFP4osc_Str LFP4osc_PFC
+  
+    % CAR (Common average reference) 
+    LFP_PFC = LFP(17:32,:) - nanmedian(LFP(17:32,:));
+    LFP_Str = LFP(1:16,:) - nanmedian(LFP(1:16,:)); 
+    clear LFP
+
+    % compute PSDs
+    disp(['computing PSD for animal number ' num2str(exp_idx)])
+    PSD_PFC = getPSD_across_timescales(LFP_PFC, fs_LFP, freq2analyze, slow_freqs, fast_freqs, ...
+        folder4PSD, experiment.animal_ID, experiment.Area1, save_data);
+    PSD_Str = getPSD_across_timescales(LFP_Str, fs_LFP, freq2analyze, slow_freqs, fast_freqs, ...
+        folder4PSD, experiment.animal_ID, experiment.Area2, save_data);
+    clear LFP_PFC LFP_Str
+end
+
+%% SDR MSC Bio 2023
+clear
+% load experiments and generic stuff
+experiments = get_experiment_redux; %function that pulls experimental indicies from your excel file
+experiments = experiments([45]); %what experiments to keepfolder4PSD = 'Q:\Personal\Tony\Analysis\Results_2Probe_PSD\'; %Fill in path where to save PSD
+
+folder4PSD = 'Q:\Personal\Tony\MScBio_20130117\psd\'; %Fill in path where to save PSD
+folder4SDR = 'Q:\Personal\Tony\MScBio_20130117\sdr\'; %folder where SDR results are saved
+
+
+% loop: go through each animal, load the PSD's, and calculate SDR 
+for exp_idx = 1 : size(experiments, 2)
+    
+    % select specific experiment for this loop iteration: 
+    experiment = experiments(exp_idx);
+    
+    load(strcat(folder4PSD, experiment.animal_ID, '_PFC'));  % load PSDstruct of PFC generated from last section 
+    PSD_PFC = nanmedian(PSDstruct.PSD);   % select only the PSD part of the whole PSDstruct
+    clear PSDstruct
+    load(strcat(folder4PSD, experiment.animal_ID, '_Str'));  % do the same for Str data  
+    PSD_Str = nanmedian(PSDstruct.PSD);
+    clear PSDstruct
+    
+    % call function to actually calculate SDR (layer-specific) 
+    disp(['calculating SDR for animal ' num2str(exp_idx)])
+    SDR = getSDR(PSD_PFC, PSD_Str, [], []);
+    
+    % save the file to appropriate folder
+    save(strcat(folder4SDR, experiment.animal_ID), 'SDR');
+end 
+
+%% Coh 
+clear
+% load experiments and generic stuff
+experiments = get_experiment_redux; %function that pulls experimental indicies from your excel file
+experiments = experiments([45]); % what experiments to keep
+repeatCalc = 1;
+save_data = 1;
+% the number of CPU cores to use to load signal. Unless you are doing
+% nothing else on your computer, do not use all of them. Otherwise you will
+% end with no processing power for anything else
+cores = 4; 
+
+% loading and filtering variables
+fs = 32000; % from data
+downsampling_factor = 100; % downsample for LFP analysis
+fs_LFP = fs / downsampling_factor;
+high_cut = fs / downsampling_factor / 2; % nyquist
+ExtractMode = 1; % extract from neuralynx into matlab
+ch2load = 1:32; % select channels to load
+
+% coherence variables
+win_length = 4; % window length for coherence (in seconds)
+overlap = win_length / 2; % window overlap for coherence (in seconds)
+nfft = 2^13; % number of points for power spectrum (coherence)
+
+% folders to save various results
+folder4Coh = 'Q:\Personal\Tony\MScBio_20130117\coh\'; 
+
+for exp_idx = 1 : size(experiments, 2)
+    
+    % select experiment
+    experiment = experiments(exp_idx);
+    
+    % load and filter first signal
+    disp(['loading LFP 1 for animal number ' num2str(exp_idx)])
+    parfor (channel = ch2load, cores) 
+        file_to_load = [experiment.path, experiment.name, '\CSC', num2str(channel), '.ncs'];
+        disp(['loading channel ' num2str(channel)])
+        [~, signal, ~] = load_nlx_Modes(file_to_load, ExtractMode, []);
+        signal = ZeroPhaseFilter(signal, fs, [0.5 high_cut]); %original lowcut 0
+        LFP(channel, :) = signal(1 : downsampling_factor : end);
+    end
+    clear signal
+    
+    noisy_channels = experiment.NoisyCh;
+    noisy_channels = noisy_channels(ismember(noisy_channels, ch2load));
+    LFP(noisy_channels, :) = NaN; %Do the same here for noisy channels.
+    
+    % keep only good part of the recording
+    if ~ nnz(isnan(experiment.RecKeep))
+        experiment.RecKeep = str2num(experiment.RecKeep);
+        LFP = LFP(:, experiment.RecKeep(1) * fs_LFP : ...
+            experiment.RecKeep(2) * fs_LFP);
+    end
+    
+    LFP1 = nanmedian(LFP(17:32,:)); %PFC signal 
+    LFP2 = nanmedian(LFP(1:16,:)); %Str signal 
+    clear LFP 
+
+    % compute coherence
+    disp(['computing coherence for animal number ' num2str(exp_idx)])
+    getCoherence(LFP1, LFP2, fs_LFP, win_length, overlap, nfft, folder4Coh, ...
+        experiment.animal_ID, experiment.Area1, experiment.Area2, repeatCalc, save_data)
+    clear LFP1 LFP2
+end
+
+%%  
+
+Str = sum(oscillations.durations) / oscillations.len_rec
+time = [PFC, Str]; 
+
+
+
+
 
 
 
--- Wrappers/MainSpikeTilingCoefficient_BetweenAreas.m
+++ Wrappers/MainSpikeTilingCoefficient_BetweenAreas.m
@@ -11,31 +11,87 @@ folder4SM1 = 'Q:\Personal\Tony\Analysis\Results_3Probe_SpikeMatrix\PFC\'; % path
 folder4SM2 = 'Q:\Personal\Tony\Analysis\Results_3Probe_SpikeMatrix\Str\'; % path to spike matrices of brain area 2
 folder4SM3 = 'Q:\Personal\Tony\Analysis\Results_3Probe_SpikeMatrix\TH3\'; % path to spike matrices of brain area 3
 
-output_folder = 'Q:\Personal\Tony\Analysis\Results_3Probe_STTC\PFC_Str'; 
+output_folder_a = 'Q:\Personal\Tony\Analysis\Results_3Probe_STTC\PFC_Str\'; 
+output_folder_b = 'Q:\Personal\Tony\Analysis\Results_3Probe_STTC\Str_TH3\'; 
+output_folder_c = 'Q:\Personal\Tony\Analysis\Results_3Probe_STTC\TH3_PFC\'; 
 
 
-
-% the animal loop, first pair
-for exp_id = 1: size(experiments, 2)
-    experiment = experiments(exp_id); 
+% the animal loop
+for exp_idx = 1: size(experiments, 2)
+    experiment = experiments(exp_idx); 
     SM1 = load([folder4SM1, experiment.animal_ID,'spike_matrix.mat']).spike_matrix; 
     SM2 = load([folder4SM2, experiment.animal_ID,'spike_matrix.mat']).spike_matrix; 
+    SM3 = load([folder4SM3, experiment.animal_ID,'spike_matrix.mat']).spike_matrix; 
     
-    % triple for-loop calculating STTC going through each pair of spike trains at each lag 
-    disp(['computing STTC for animal ', num2str(exp_idx), ' out of ', size(experiments,2)])
+    rec_length = max([size(SM1,2), size(SM2,2), size(SM3,2)])/1000; % set recording length and cover to seconds per requried by getSTTC 
+    rec_time = [0 rec_length]; 
+    
+    % three separate triple for-loops calculating STTC going through each pair of spike trains at each lag
+    % first area pair: PFC_Str
+    disp(['computing STTC for animal ', num2str(exp_idx), ' out of ', num2str(size(experiments,2))])
     for lag_idx = 1: length(lags) %the lag loop 
         pair_idx = 1; 
-        lag = lags(lag_idx); 
+        lag = lags(lag_idx)/1000; % select lag time and convert to seconds per required by getSTTC  
         for SM1_idx = 1: size(SM1,1) % the SM1 loop 
-            spike_times_1 = find(SM1(SM1_idx,:));
+            spike_times_1 = find(SM1(SM1_idx,:))/1000; % extract timestamps of spike train 1 and convert to seconds 
             spike_counts_1 = size(spike_times_1, 2); 
-            for SM2_idx = 1: size(SM2,2) % the SM2 loop 
-                spike_times_2 = find(SM2(SM2_idx,:)); 
+            for SM2_idx = 1: size(SM2,1) % the SM2 loop 
+                spike_times_2 = find(SM2(SM2_idx,:))/1000; %extract timestamps of spike train 2 and convert to seconds 
                 spike_counts_2 = size(spike_times_2,2); 
-                STTC(pair_idx, lag_idx) = getSTTC( , , lag, , , , ) % use the function to calculate the pair
+                STTC(pair_idx, lag_idx) = getSTTC(spike_counts_1, spike_counts_2, lag, rec_time, spike_times_1, spike_times_2); 
                 pair_idx = pair_idx + 1; 
             end
         end 
     end 
+    % put everything in a structure and save 
+    Tcoeff.STTC = STTC; 
+    Tcoeff.lags = lags; 
+    Tcoeff.areas = 'PFC_Str'; 
+    save(strcat(output_folder_a, experiment.animal_ID), 'Tcoeff'); 
+    clear STTC Tcoeff
+   
+    % second area pair: Str_TH3
+    for lag_idx = 1: length(lags) %the lag loop 
+        pair_idx = 1; 
+        lag = lags(lag_idx)/1000; % select lag time and convert to seconds per required by getSTTC  
+        for SM2_idx = 1: size(SM2,1) % the SM2 loop 
+            spike_times_2 = find(SM2(SM2_idx,:))/1000; % extract timestamps of spike train 2 and convert to seconds 
+            spike_counts_2 = size(spike_times_2, 2); 
+            for SM3_idx = 1: size(SM3,1) % the SM3 loop 
+                spike_times_3 = find(SM3(SM3_idx,:))/1000; % extract timestamps of spike train 3 and convert to seconds 
+                spike_counts_3 = size(spike_times_3,2); 
+                STTC(pair_idx, lag_idx) = getSTTC(spike_counts_2, spike_counts_3, lag, rec_time, spike_times_2, spike_times_3); 
+                pair_idx = pair_idx + 1; 
+            end
+        end 
+    end 
+    % put everything in a structure and save 
+    Tcoeff.STTC = STTC; 
+    Tcoeff.lags = lags; 
+    Tcoeff.areas = 'Str_TH3'; 
+    save(strcat(output_folder_b, experiment.animal_ID), 'Tcoeff'); 
+    clear STTC Tcoeff
+    
+    % third area pair: TH3_PFC
+    for lag_idx = 1: length(lags) %the lag loop 
+        pair_idx = 1; 
+        lag = lags(lag_idx)/1000; % select lag time and convert to seconds per required by getSTTC  
+        for SM1_idx = 1: size(SM1,1) % the SM1 loop 
+            spike_times_1 = find(SM1(SM1_idx,:))/1000; % extract timestamps of spike train 1 and convert to seconds 
+            spike_counts_1 = size(spike_times_1, 2); 
+            for SM3_idx = 1: size(SM3,1) % the SM3 loop 
+                spike_times_3 = find(SM3(SM3_idx,:))/1000; %extract timestamps of spike train 3 and convert to seconds 
+                spike_counts_3 = size(spike_times_3,2); 
+                STTC(pair_idx, lag_idx) = getSTTC(spike_counts_1, spike_counts_3, lag, rec_time, spike_times_1, spike_times_3); 
+                pair_idx = pair_idx + 1; 
+            end
+        end 
+    end 
+    % put everything in a structure and save 
+    Tcoeff.STTC = STTC; 
+    Tcoeff.lags = lags; 
+    Tcoeff.areas = 'TH3_PFC'; 
+    save(strcat(output_folder_c, experiment.animal_ID), 'Tcoeff'); 
+    clear STTC Tcoeff
 end 
 
